654385e10254aa3642a3c8e7d36449528f844b9c
mr-universe-guy <ff8loser@gmail.com>
2024-09-30 21:44:20 -0400

    Added various classes to use as span styles in text setText() changed to displayText() and appends the given text to the bottom of the text area Modified the scenes for act1

--- FullMoonGame/FullMoon.js
+++ FullMoonGame/FullMoon.js
@@ -7,12 +7,25 @@ async function loadScenes(actData) {
 }
 
 const scenes = {};
-let currentSceneId = "CargoBayCh1-1"; //This is the starting scenes
+let currentSceneId = "Ch0-CargoBay"; //This is the starting scenes
+const inventory = [];
 let lycanthropy = 0.0; //Track how far along the transformation is
 
+function clearDisplayedText(){
+	document.getElementById("TextDisplayArea").innerHTML = ""
+}
+
 //TODO: Have text be an array that breaks into lines. Also have it typed out for funsies
-function setText(text) {
-	document.getElementById("TextDisplayArea").innerHTML = text;
+function displayText(text) {
+	const disp = document.createElement('div');
+	disp.classList.add("text-entry");
+	disp.innerHTML = text;
+	const displayArea = document.getElementById("TextDisplayArea");
+	displayArea.append(disp);
+	if(displayArea.childNodes.length > 25){
+		displayArea.removeChild(displayArea.lastChild);
+	}
+	displayArea.scrollTop = displayArea.scrollHeight;
 }
 
 // Attempt to set the current scenes from a given scenesId
@@ -20,7 +33,7 @@ function setScene(sceneId) {
 	const scene = scenes[sceneId];
 	if(scene){
 		currentSceneId = sceneId;
-		setText(scene.description);
+		displayText(scene.description);
 		//set each available action
 		let actionArea = document.getElementById("AvailableActionsArea");
 		actionArea.innerHTML = "<ul>"
@@ -31,7 +44,7 @@ function setScene(sceneId) {
 		}
 		actionArea.innerHTML += "<li>return</li></ul>"
 	} else{
-		setText("Scenes not found. "+sceneId);
+		displayText("Scenes not found. "+sceneId);
 	}
 }
 
@@ -39,7 +52,7 @@ function runAction(type, value){
 	//console.log("Running action :"+type+","+value)
 	switch(type){
 		case "description":
-			setText(value);
+			displayText(value);
 			break;
 		case "changeLocation":
 			setScene(value);
@@ -54,6 +67,8 @@ function parseUserInput(){
 	const inputField = document.getElementById("UserInput");
 	const input = inputField.value.trim();
 	inputField.value = "";
+	//display the user input
+	displayText("<span class='user-input'>--"+input+"</span>");
 	//there are a few inputs that are always available, handle them here
 	switch(input){
 		case "return":
@@ -76,6 +91,7 @@ function detectEnter(e){
 }
 
 async function init() {
+	clearDisplayedText();
 	const loadedScenes = await loadScenes('resources/Act1Data.json');
 	Object.assign(scenes, loadedScenes);
 	setScene(currentSceneId);
